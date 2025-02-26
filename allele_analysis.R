a3138bb1779b6c13d188e11b5f50f78b9e6323b1
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2025-02-26 12:25:53 -0500

    Update allele_analysis.R

--- allele_analysis.R
+++ allele_analysis.R
@@ -75,69 +75,6 @@ alfreq_data[, sampTime:=ifelse(gen_season=="EG",(20*year)%/%9000, (12*year)%/%90
 alfreq_data[Freq.bin=="Segregating",fc_year:=max(mut_freq)-min(mut_freq), by=c("id", "year")] # yearly frequency change of segregating alleles
 alfreq_data[Gen>=10000, fluctuating:=ifelse(mut_freq!=0 & mut_freq != 1, T, F), by="id"] # determine if fluctuating after initial establishment period 
 
-## Figure S1 - Distribution of mean effect size for segregating and fixed/lost alleles.
-lostloc=unique(alfreq_data[Gen==18000&Freq.bin=="Segregating", .(id, y, l)]) #videntify loci segregating at final sampling gen
-vals=na.omit(alfreq_data[Gen==1, .(Gen, id, s_d, w_d, s_fx, w_fx, y,l)]) # remove NA values
-vals[, finalseg:="Fixed"] # give all "Fixed" label
-vals[id %in% lostloc$id, finalseg:="Segregating"] # labels segregating loci as segregating
-vals[, meanfx:= (s_fx/w_fx), by=c("id","y", "l")] # average effect size
-vals[, y_lab:=paste0("y = ",y), by="y"] # add y label
-vals[, l_lab:=paste0("l = ",l), by="l"] # add l label
-
-y_labels=c("y = 0.5", "y = 1","y = 2","y = 4","y = 8", "y = 12", "y = 16","y = 20")
-names(y_labels)<-c("0.5", "1", "2", "4","8","12","16","20")
-
-plot=ggplot()+ geom_density(data=vals[finalseg=="Fixed"], aes(x=meanfx), col="red")+ 
-  geom_density(data=vals[finalseg=="Segregating"], aes(x=meanfx))+ facet_wrap(~l_lab+y_lab) +
-  scale_x_log10()+ labs(x="Seasonal Effect Size Ratio", y="Density") + theme_bw()
-ggsave(filename =paste0("plots/FigureS1.jpg"), plot = plot , width = 8, height = 6)
-ggsave(filename =paste0("plots/FigureS1.pdf"), plot = plot , width = 8, height = 6)
-
-## Figure 1 - The average number of segregating loci across time.
- 
-rm.col=c("s_d", "w_d", "s_fx", "w_fx") # remove dominance and effect size columns
-alfreq_data[, (rm.col):=NULL]
- # calculate minimum and maximum frequency in year/seasonal cycle
-intermediate = alfreq_data[Freq.bin=="Segregating", .(ymax = max(mut_freq), ymin = min(mut_freq)), by=c("id", "year","sampTime","group", "label", "env", "y", "l") ]
- # calculate amplitude across year
-intermediate[, yamp:=ymax-ymin, by=c("id", "year","sampTime","group", "label") ]
- # calculate max, min and mean amplitude across replicates
-amplitudes = intermediate[yamp>0, .(av_max = mean(ymax), av_min = mean(ymin), av_amp = mean(yamp)), by=c("id","group", "label", "sampTime", "env", "y", "l")]
- # average amplitude for each parameter set
-amplitudes[, amp := av_max-av_min, c("id", "group","label", "sampTime", "env", "y")]
-amplitudes[, year:=as.integer(sampTime)*9000, by="sampTime"]  # set year
-amplitudes[, clean_lab:=paste0("l = ", l), by= "l"] # add l label
-
-segloc_data=unique(na.omit(alfreq_data[Gen>1, .(Gen, n_seg, y, id, env, l)])) ## isolate unique mutation information
-segloc_data[, n_seg_rep:=n_seg/20, by=c("Gen", "y", "l" )] # avergae the number of segregating loci across replicates
-segdata=unique(segloc_data[Gen<28000, .(Gen, y, l, n_seg_rep)]) #remove any additional duplicates
-segdata[, clean_l:=paste0("l = ", l), by = "l"] # add l label
-# create empty data.table for figure
-dummy <- data.table(Gen = c(0, 27000, 0, 27000, 0, 27000 ), n_seg = c(0, 100, 0, 200, 0, 500), l=c(100,100,200,200,500,500))
-
-l_labels<-c("l = 100", "l = 200", "l = 500") # set labels
-names(l_labels)<-c("100", "200", "500")
-Fig1 = ggplot(segdata, aes(x = Gen)) + 
-  geom_blank(data=dummy, aes(x=Gen, y=n_seg))+
-  geom_line(aes(y=n_seg_rep,  col=factor(y)), alpha=0.8) +
-  theme_bw()+ coord_cartesian(xlim=c(0,17000))+
-  labs(x="Generations",y="Number of Segregating Loci", col="Epistasis")+facet_wrap(~l, scale="free_y", labeller = labeller(l=l_labels))
-
-ggsave(filename =paste0("plots/Figure1.pdf"), plot = Fig1 , width = 11, height = 6)
-ggsave(filename =paste0("plots/Figure1.jpg"), plot = Fig1 , width = 11, height = 6)
-
-## Figure 2 - Distribution of the amplitude of allele frequency fluctuations across time. 
-
-amplitudes[, clean_y:=paste0("y = ", y), by= "y"] # add y label
-
-al.dist = ggplot(amplitudes[y>1 & year<20000 ],aes(x = amp, col = factor(year))) + 
-  geom_density() + scale_x_log10()+
-  theme_bw()+
-  labs(x="Amplitude of Fluctuations", y="Density", col= "Year")+ facet_wrap(~clean_lab+factor(clean_y, levels = c("y = 2","y = 4", "y = 8", "y = 12", "y = 16", "y = 20")), nrow=6)
-
-ggsave(filename="plots/Figure2.pdf", al.dist, width = 8, height =8)
-ggsave(filename="plots/Figure2.jpg", al.dist, width = 8, height =8)
-
 ## Figure 3 - Relationship between number of segregating loci, epistasis, and amplitude of seasonal allele frequency fluctuation.
 # calculate average max, mean and minimum allele frequency
 # calculate minimum and maximum frequency in year/seasonal cycle
@@ -151,20 +88,20 @@ amps[, amp := av_max-av_min, c("id", "group","label", "sampTime", "env", "y","ru
 amps[, year:=as.integer(sampTime)*9000, by="sampTime"] # set year
 nseg.scaled=amps[, .N, by=c("label","run", "year", "env","l","y")] # count the number of segregating loci at each time point
 # calculate the mean, variance, median and 90% quantile of amplitude for each replicate
-amps[, `:=` (mean=mean(amp), var=var(amp), med=median(amp), nq=quantile(amp, probs=0.9)), by=c("label","run", "year", "env","y")]
+amps[, `:=` (mean=mean(amp), var=var(amp), med=median(amp), max=max(amp),nq=quantile(amp, probs=0.9)), by=c("label","run", "year", "env","y")]
 # merge data sets for plotting
-nseg.scaled=merge(nseg.scaled, unique(amps[, .(group, label, run, year, mean, var, med, nq)]), by=c("label", "run", "year"))
+nseg.scaled=merge(nseg.scaled, unique(amps[, .(group, label, run, year, mean, var, med,max, nq)]), by=c("label", "run", "year"))
 
-# rename mean, median and 90% Quartile labels
-xx <- names(nseg.scaled); xx[9] <- "Mean"; xx[11] <-"Median"; xx[12] <-"90% Quartile"
+# rename mean, median,max  and 90% Quartile labels
+xx <- names(nseg.scaled); xx[9] <- "Mean"; xx[11] <-"Median"; xx[12] <-"90% Quartile"; xx[13] <- "Maximum"
 setnames(nseg.scaled, xx)
 # transform data set to plot in one figure
-fig3.data=melt(nseg.scaled[,c(1:9,11:12)], measure.vars = c("Mean", "Median", "90% Quartile"), variable.name = "stat")
+fig1.data=melt(nseg.scaled[,c(1:9,11:13)], measure.vars = c("Mean", "Median", "Maximum","90% Quartile"), variable.name = "stat")
 
-fig3.data[, clean_l:=paste0("l = ", l), by= "l"] # set labels
-fig3.data[, clean_y:=paste0("y = ", y), by= "y"]
+fig1.data[, clean_l:=paste0("l = ", l), by= "l"] # set labels
+fig1.data[, clean_y:=paste0("y = ", y), by= "y"]
 
-fig.3=ggplot(fig3.data[year==18000],aes(x=N, y=value,col=factor(y)))+
+fig.1=ggplot(fig1.data[year==18000],aes(x=N, y=value,col=factor(y)))+
   geom_point(aes(shape=factor(l)))+
   geom_smooth(aes(col=factor(y)),method='lm', formula= y~x, se = FALSE, alpha=0.3)+  
   facet_wrap(~stat, ncol=1, strip.position = 'right')+
@@ -172,8 +109,8 @@ fig.3=ggplot(fig3.data[year==18000],aes(x=N, y=value,col=factor(y)))+
   labs(x="Final Number of Segregating Loci", y="Amplitude of Allele Frequency Fluctuation", col="Epistasis (y)", shape=
          "Initial Loci\nNumber")+ scale_x_log10()+ scale_y_log10()
 
-ggsave(filename =paste0("plots/Figure3.jpg"), plot = fig.3 , width = 8, height = 7)
-ggsave(filename =paste0("plots/Figure3.pdf"), plot = fig.3 , width = 8, height = 7)
+ggsave(filename =paste0("plots/Figure1.pdf"), plot = fig.1 , width = 11, height = 6)
+ggsave(filename =paste0("plots/Figure1.jpg"), plot = fig.1 , width = 11, height = 6)
 
 ## Multiple regression models and sampling empirically-based simulation parameters
 
